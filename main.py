4dc1dd6f5e6efa09b35e086130665eb0700f5cb8
Sebas2476 <Sebastian_ramos5@outlook.com>
2025-08-05 22:00:52 -0500

    Completion of project

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from stats import num_of_words
+import sys
+from stats import num_of_words, num_of_chars, sort_func 
 def get_book_text(filepath):
     with open(filepath) as f:
         text = f.read()
         return text
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
-    print(book)
-    result = num_of_words(book)
-    print(result)
+     if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+     else:
+        book_path = sys.argv[1]
+        book =  get_book_text(book_path)
+        result = num_of_words(book)
+        sort_dicts = sort_func(book)
+        print("============ BOOKBOT ============")
+        print(book)
+        print("----------- Word Count ----------")
+        print(result)
+        print("--------- Character Count -------")
+        for sort_dict in sort_dicts:
+            print(f"{sort_dict["char"]}: {sort_dict["num"]}")
+            
+        print("============= END ===============")
+    
+   
+    
 
 main()
--- stats.py
+++ stats.py
@@ -4,8 +4,28 @@ def num_of_words(result):
     splits = result.split()
     for split in splits:
         num_words += 1
-    return f"{num_words} words found in the document"
+    
+    message = f"Found {num_words} total words"
+    return message
 
 
+def num_of_chars(book_for_dict):
+    symbol_dict = {}
+    words = book_for_dict.lower()
+    for charr in words:
+        if charr not in symbol_dict:
+            symbol_dict[charr] = 1
+        else:
+            symbol_dict[charr] += 1
+    return symbol_dict
+
+def sort_func(input_for_dict): 
+    sort_list = []
+    dicts = num_of_chars(input_for_dict)
+    for key, value in dicts.items():
+        if key.isalpha():
+            sort_list.append({"char": key, "num": value})
+    sort_list.sort(reverse=True, key=lambda item: item["num"] )
+    return sort_list
 
 
